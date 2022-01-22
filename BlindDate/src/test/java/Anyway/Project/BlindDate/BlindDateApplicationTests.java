3e1eda10d1c159c05f33a82a2020e051c43009f1
woominsik <9737638@naver.com>
2022-01-22 21:27:17 +0900

    Add files via upload
    fist commit

---
+++ BlindDate/src/test/java/Anyway/Project/BlindDate/BlindDateApplicationTests.java
@@ -0,0 +1,13 @@
+package Anyway.Project.BlindDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
